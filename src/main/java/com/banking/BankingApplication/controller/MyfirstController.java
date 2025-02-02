556abd111348deca37677f82ec9b614af9e37a0e
Tapabrata <tapabratadas40@gmail.com>
2025-02-02 08:16:49 +0530

    ignore commit

--- src/main/java/com/banking/BankingApplication/controller/MyfirstController.java
+++ src/main/java/com/banking/BankingApplication/controller/MyfirstController.java
@@ -1,6 +1,13 @@
 package com.banking.BankingApplication.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/")
 public class MyfirstController {
+	
+	public void myFirstController() {
+		System.out.println("akash");
+	}
 	
 
 }
